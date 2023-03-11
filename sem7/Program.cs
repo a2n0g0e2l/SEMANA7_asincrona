f9de640ed705c88fc75bc10a3ec1f3f0098920f2
a2n0g0e2l <125489061+a2n0g0e2l@users.noreply.github.com>
2023-03-11 04:52:00 -0600

    Terminado

---
+++ sem7/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+static void Main (string[]args)
+{
+    Console.Write("Introduce un numero entero");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 1 ; i <= num; i++)
+    {
+        for (int h = 1; h <= i; h++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
